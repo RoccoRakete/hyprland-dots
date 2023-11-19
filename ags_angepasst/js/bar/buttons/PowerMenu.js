55ea96b1dc0de98eac9c1aa508d67056a95eec86
RoccoRakete <m.schaumann@mscloud.uk>
2023-11-19 12:10:02 +0100

    update

--- ags_angepasst/js/bar/buttons/PowerMenu.js
+++
@@ -1,9 +0,0 @@
-import icons from '../../icons.js';
-import PanelButton from '../PanelButton.js';
-import { Widget, App } from '../../imports.js';
-
-export default () => PanelButton({
-    class_name: 'powermenu',
-    content: Widget.Icon(icons.powermenu.shutdown),
-    onClicked: () => App.openWindow('powermenu'),
-});
--- ags_angepasst/js/misc/RegularWindow.js
+++
@@ -1,4 +0,0 @@
-import Gtk from 'gi://Gtk';
-import Widget from 'resource:///com/github/Aylur/ags/widget.js';
-
-export default Widget.subclass(Gtk.Window, 'RegularWindow');
