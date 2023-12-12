dbc880c9712173cf534584742af8a11542310465
RoccoRakete <m.schaumann@mscloud.uk>
2023-12-12 17:51:19 +0100

    ....

--- nixos/configs/global-configs/tlp.nix
+++ nixos/configs/global-configs/tlp.nix
@@ -5,7 +5,7 @@
     tlp.enable = true;
     tlp.settings = {
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_SCALING_GOVERNOR_ON_AC = "ondemand";
+      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
       CPU_ENERGY_PERF_POLICY_ON_AC = "default";
       CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
       CPU_BOOST_ON_AC = 1;
